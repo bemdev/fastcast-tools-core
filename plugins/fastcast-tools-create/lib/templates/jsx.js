3263e0d70a21fedbf712838b683a2177dfcf91c6
bemdev <bemdev@mediaforge.Dlink>
2021-09-17 19:18:19 +0400

    modify template to bem methodology

--- plugins/fastcast-tools-create/lib/templates/jsx.js
+++ plugins/fastcast-tools-create/lib/templates/jsx.js
@@ -2,39 +2,56 @@
 
 var EOL = require('os').EOL
 
+function toCamelCase(str) {
+    return str.replace(str[0], str[0].toUpperCase());
+}
+
 module.exports = function (entity, naming) {
 	let first
 
 	if (entity.mod?.name) {
+    var capitalizeBlock = toCamelCase(entity.block)
+
 		var modVal =
 			typeof entity.mod.val === 'boolean' ? entity.mod.val : "'" + entity.mod.val + "'"
 
+    var valBoolean = typeof entity.mod.val === 'boolean' 
+
 		first = [
-			"import React from 'reactify'",
+      !valBoolean ? "import React from 'reactify'" : '//without react',
+			"import { withBemMod } from '@bem-react/core'",
 			'',
-			`import './${entity.block}.css'`,
-			'',
-			`const ${entity.block} = (props) => <div className={React.cn('${entity.block}')({ ${entity.mod.name}: ${modVal} })}></div>`,
-			'',
-			`export default ${entity.block}`,
+      `import './${entity.block}_${entity.mod.name}${!valBoolean ? '_' + entity.mod.val : ''}.css'`,
+			`import { blockName } from '../${entity.block}/${entity.block}'`,
+      !valBoolean ? '' : null,
+      !valBoolean ? `const ${capitalizeBlock}${toCamelCase(entity.mod.name)} = ({children}) => <div className={blockName({ ${entity.mod.name}: ${modVal} })}>{children}</div>` : '//primitive application with mods and css',
+      entity.mod.name
+      ? `export const ${capitalizeBlock}${toCamelCase(entity.mod.name)}${!valBoolean ? toCamelCase(entity.mod.val) : ''} = withBemMod(blockName(), { ${entity.mod.name}: ${modVal} }${!valBoolean ? ',' + capitalizeBlock : ''}${!valBoolean ? toCamelCase(entity.mod.name) : ''})`
+          : `export default ${entity.block}`,
+      ''
 		]
 	} else if (entity.elem) {
+    var capitalizeElem = toCamelCase(entity.elem)
+
 		first = [
 			"import React from 'reactify'",
+      '',
+			`import { blockName } from '../${entity.block}/${entity.block}'`,
+			`import './${entity.block}__${entity.elem}.css'`,
+			`const elemName = React.cn(blockName(), '${entity.elem}')()`,
 			'',
-			`import './${entity.elem}.css'`,
+			`const ${capitalizeElem} = ({children}) => <div className={elemName}>{children}</div>`,
 			'',
-			`const ${entity.elem} = (props) => <div className={React.cn('${entity.block}', '${entity.elem}')()}></div>`,
-			'',
-			`export default ${entity.elem}`,
+			`export default ${capitalizeElem}`,
 		]
 	} else {
 		first = [
 			"import React from 'reactify'",
 			'',
 			`import './${entity.block}.css'`,
+			`export const blockName = React.cn('${entity.block}')`,
 			'',
-			`const ${entity.block} = (props) => <div className={React.cn('${entity.block}')()}></div>`,
+			`const ${entity.block} = ({children}) => <div className={blockName()}>{children}</div>`,
 			'',
 			`export default ${entity.block}`,
 		]
